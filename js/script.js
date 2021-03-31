317edff1040dce193bab359dbdf601d0663edfc5
semih <semih.ucak@student.odisee.be>
2021-03-31 17:15:55 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,5 @@
+import * as THREE from 'https://unpkg.com/three@0.123.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.123.0/examples/jsm/loaders/GLTFLoader.js';
+import { VRButton } from '../three/examples/jsm/webxr/VRButton.js';
+
