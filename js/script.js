9d2d2f631a0122f909a911058a3ac52c490affba
semih <semih.ucak@student.odisee.be>
2021-04-01 12:30:40 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,35 +1,152 @@
-import * as THREE from 'https://unpkg.com/three@0.123.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.123.0/examples/jsm/loaders/GLTFLoader.js';
 import { VRButton } from '../three/examples/jsm/webxr/VRButton.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 10000);
-const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-document.body.appendChild( VRButton.createButton( renderer ) );
-renderer.xr.enabled = true;
+import * as THREE from '../js/three.module.js';
+import { ARButton } from '../jsm/webxr/ARButton.js';
 
-const geometry = new THREE.BoxGeometry(500, 500, 500);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: false});
-const cube = new THREE.Mesh(geometry, material);
+let container;
+let camera, scene, renderer;
+let controller;
 
-scene.add(cube);
-camera.position.z = 1000;
+let reticle;
 
-function render() {
-  requestAnimationFrame(render);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-};
+let hitTestSource = null;
+let hitTestSourceRequested = false;
 
-renderer.setAnimationLoop( function () {
+init();
+animate();
 
-	renderer.render( scene, camera );
+function init() {
 
-} );
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
 
-render();
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
+
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    light.position.set( 0.5, 1, 0.25 );
+    scene.add( light );
+
+    //
+
+    renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.xr.enabled = true;
+    container.appendChild( renderer.domElement );
+
+    //
+
+    document.body.appendChild( ARButton.createButton( renderer, { requiredFeatures: [ 'hit-test' ] } ) );
+
+    //
+
+    const geometry = new THREE.CylinderGeometry( 0.1, 0.1, 0.2, 32 ).translate( 0, 0.1, 0 );
+
+    function onSelect() {
+
+        if ( reticle.visible ) {
+
+            const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
+            const mesh = new THREE.Mesh( geometry, material );
+            mesh.position.setFromMatrixPosition( reticle.matrix );
+            mesh.scale.y = Math.random() * 2 + 1;
+            scene.add( mesh );
+
+        }
+
+    }
+
+    controller = renderer.xr.getController( 0 );
+    controller.addEventListener( 'select', onSelect );
+    scene.add( controller );
+
+    reticle = new THREE.Mesh(
+        new THREE.RingGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
+        new THREE.MeshBasicMaterial()
+    );
+    reticle.matrixAutoUpdate = false;
+    reticle.visible = false;
+    scene.add( reticle );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+//
+
+function animate() {
+
+    renderer.setAnimationLoop( render );
+
+}
+
+function render( timestamp, frame ) {
+
+    if ( frame ) {
+
+        const referenceSpace = renderer.xr.getReferenceSpace();
+        const session = renderer.xr.getSession();
+
+        if ( hitTestSourceRequested === false ) {
+
+            session.requestReferenceSpace( 'viewer' ).then( function ( referenceSpace ) {
+
+                session.requestHitTestSource( { space: referenceSpace } ).then( function ( source ) {
+
+                    hitTestSource = source;
+
+                } );
+
+            } );
+
+            session.addEventListener( 'end', function () {
+
+                hitTestSourceRequested = false;
+                hitTestSource = null;
+
+            } );
+
+            hitTestSourceRequested = true;
+
+        }
+
+        if ( hitTestSource ) {
+
+            const hitTestResults = frame.getHitTestResults( hitTestSource );
+
+            if ( hitTestResults.length ) {
+
+                const hit = hitTestResults[ 0 ];
+
+                reticle.visible = true;
+                reticle.matrix.fromArray( hit.getPose( referenceSpace ).transform.matrix );
+
+            } else {
+
+                reticle.visible = false;
+
+            }
+
+        }
+
+    }
+
+    renderer.render( scene, camera );
+
+}
