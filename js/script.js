f2b051703817fafee82deb0ec96ef1a83e766eb6
semih <semih.ucak@student.odisee.be>
2021-04-01 15:57:08 +0200

    update code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-import { OrbitControls } from 'https://unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.123.0/examples/jsm/loaders/GLTFLoader.js';
-import { VRButton } from '../three/examples/jsm/webxr/VRButton.js';
-
-
-import * as THREE from '../js/three.module.js';
+import * as THREE from '../build/three.module.js';
 import { ARButton } from '../jsm/webxr/ARButton.js';
+import { OrbitControls } from '../jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from '../jsm/loaders/RGBELoader.js';
+import { RoughnessMipmapper } from '../jsm/utils/RoughnessMipmapper.js';
 
 let container;
 let camera, scene, renderer;
 let controller;
 
-let reticle;
+let reticle,pmrerGenerator,current_object,controls;
 
 let hitTestSource = null;
 let hitTestSourceRequested = false;
@@ -18,18 +17,64 @@ let hitTestSourceRequested = false;
 init();
 animate();
 
+$(".ar-object").click(function(){
+    if(current_object != null){
+        scene.remove(current_object);
+    }
+
+    loadModel($(this).attr("id"));
+});
+
+
+$("#ARButton",function(){
+    current_object.visible = false;
+});
+
+function loadModel(model){
+
+    new RGBELoader()
+    .setDataType(THREE.UnsignedByteType)
+    .setPath('textures/')
+    .load('photo_studio_01_1k.hdr', function(texture){
+
+        var envmap = pmrerGenerator.fromEquirectangular(texture).texture;
+
+        scene.environment = envmap;
+        texture.dispose();
+        pmrerGenerator.dispose();
+        render();
+
+        var loader = new GLTFLoader().setPath('models/');
+        loader.load(model + ".glb", function(glb){
+
+            current_object = glb.scene;
+            scene.add(current_object);
+
+            var box = new THREE.Box3();
+            box.setFromObject(current_object);
+            box.center(controls.target);
+
+            render();
+        });
+    });
+
+}
+
 function init() {
 
     container = document.createElement( 'div' );
-    document.body.appendChild( container );
+    document.getElementById( "container" ).appendChild(container);
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 200 );
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    light.position.set( 0.5, 1, 0.25 );
-    scene.add( light );
+    var directionalLight = new THREE.DirectionalLight(0xdddddd, 1);
+	directionalLight.position.set(0, 0, 1).normalize();
+	scene.add(directionalLight);
+
+	var ambientLight = new THREE.AmbientLight(0x222222);
+	scene.add(ambientLight);
 
     //
 
@@ -38,6 +83,17 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.xr.enabled = true;
     container.appendChild( renderer.domElement );
+
+    pmrerGenerator = new THREE.PMREMGenerator(renderer);
+    pmrerGenerator.compileEquirectangularShader();
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render);
+    controls.minDistance = 2;
+    controls.maxDistance = 10;
+    controls.target.set(0,0,-0.2);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
 
     //
 
@@ -51,11 +107,14 @@ function init() {
 
         if ( reticle.visible ) {
 
-            const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
+           /* const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
             const mesh = new THREE.Mesh( geometry, material );
             mesh.position.setFromMatrixPosition( reticle.matrix );
             mesh.scale.y = Math.random() * 2 + 1;
-            scene.add( mesh );
+            scene.add( mesh ); */
+
+            current_object.position.setFromMatrixPosition(reticle.matrix);
+            current_object.visible = true;
 
         }
 
@@ -93,6 +152,9 @@ function onWindowResize() {
 function animate() {
 
     renderer.setAnimationLoop( render );
+    requestAnimationFrame(animate);
+    controls.update();
+
 
 }
 
@@ -119,6 +181,12 @@ function render( timestamp, frame ) {
 
                 hitTestSourceRequested = false;
                 hitTestSource = null;
+
+                reticle.visible = false;
+
+                var box = new THREE.Box3();
+                box.setFromObject(current_object);
+                box.center(controls.target);
 
             } );
 
